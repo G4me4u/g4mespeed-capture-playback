6bc4ae4e3f5ee68248e6e2ba14f7de4de6e0111f
G4me4u <G4me4u@gmail.com>
2020-06-28 21:05:02 +0200

    Removed position ui

--- src/main/java/com/g4mesoft/captureplayback/gui/GSCapturePlaybackGUI.java
+++ src/main/java/com/g4mesoft/captureplayback/gui/GSCapturePlaybackGUI.java
@@ -9,28 +9,18 @@ import net.minecraft.client.gui.widget.ButtonWidget;
 public class GSCapturePlaybackGUI extends GSParentPanel {
 
 	private static final int TOP_MARGIN = 5;
-	private static final int POSITION_GUI_HEIGHT = 16;
 
 	private final GSCapturePlaybackModule module;
 	
-	private final GSPositionGUI positionGUI;
-	
 	public GSCapturePlaybackGUI(GSCapturePlaybackModule module) {
 		this.module = module;
-		
-		positionGUI = new GSPositionGUI();
 	}
 	
 	@Override
 	public void init() {
 		super.init();
 		
-		int y = TOP_MARGIN;
-		positionGUI.initBounds(client, 0, y, width / 2, POSITION_GUI_HEIGHT);
-		addPanel(positionGUI);
-		y += POSITION_GUI_HEIGHT + 5;
-
-		addWidget(new ButtonWidget(width / 2 - 45, y, 90, 20, "Edit timeline", (button) -> {
+		addWidget(new ButtonWidget(width / 2 - 45, TOP_MARGIN, 90, 20, "Edit timeline", (button) -> {
 			client.openScreen(new GSEditTimelineGUI(module.getActiveTimeline(), module));
 		}));
 	}
