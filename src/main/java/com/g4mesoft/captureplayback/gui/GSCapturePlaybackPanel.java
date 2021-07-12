aa115356d6c7ccf6fc642e29a2155b071851408a
G4me4u <G4me4u@gmail.com>
2021-07-12 13:15:29 +0200

    Refactored GSMainGUI to GSContentHistoryGUI

--- src/main/java/com/g4mesoft/captureplayback/gui/GSCapturePlaybackPanel.java
+++ src/main/java/com/g4mesoft/captureplayback/gui/GSCapturePlaybackPanel.java
@@ -4,7 +4,7 @@ import com.g4mesoft.captureplayback.composition.GSComposition;
 import com.g4mesoft.captureplayback.module.GSCompositionSession;
 import com.g4mesoft.captureplayback.module.client.GSCapturePlaybackClientModule;
 import com.g4mesoft.core.client.GSClientController;
-import com.g4mesoft.gui.GSMainGUI;
+import com.g4mesoft.gui.GSContentHistoryGUI;
 import com.g4mesoft.panel.GSParentPanel;
 import com.g4mesoft.panel.legend.GSButtonPanel;
 
@@ -16,13 +16,13 @@ public class GSCapturePlaybackPanel extends GSParentPanel {
 
 	public GSCapturePlaybackPanel(GSCapturePlaybackClientModule module) {
 		editCompositionButton = new GSButtonPanel("Edit Composition", () -> {
-			GSMainGUI mainGUI = GSClientController.getInstance().getMainGUI();
+			GSContentHistoryGUI primaryGUI = GSClientController.getInstance().getPrimaryGUI();
 			
 			GSCompositionSession session = module.getCompositionSession();
 			GSComposition composition = module.getSessionComposition();
 			
 			if (session != null && composition != null)
-				mainGUI.setContent(new GSCompositionEditPanel(module, session, composition));
+				primaryGUI.setContent(new GSCompositionEditPanel(module, session, composition));
 		});
 		
 		add(editCompositionButton);
